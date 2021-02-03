9a4728a505fd4a0efa8299d93fc6a361d394cbd9
Yuta Imai <imai.factory@gmail.com>
2021-02-03 21:45:00 +0900

    Add random food location update with threads

--- src/food.cpp
+++ src/food.cpp
@@ -10,6 +10,7 @@ void Food::UpdateLocation() {
     // Check that the location is not occupied by a snake item before placing
     // food.
     if (!snake->SnakeCell(x, y)) {
+      std::lock_guard<std::mutex> lock(mtx);
       location.x = x;
       location.y = y;
       return;
@@ -18,9 +19,32 @@ void Food::UpdateLocation() {
 }
 
 bool Food::IsAt(int target_x, int target_y){
+  std::lock_guard<std::mutex> lock(mtx);
   return (location.x == target_x && location.y == target_y);
 }
 
 SDL_Point Food::GetLocation() {
-  return location;
+  std::lock_guard<std::mutex> lock(mtx);
+  SDL_Point result = location;
+  return result;
+}
+
+void Food::RandomLocationUpdate() {
+  const int phase_duration_from  = 4;
+  const int phase_duration_to    = 6;
+  std::uniform_int_distribution<int>  distr(phase_duration_from, phase_duration_to);
+  std::chrono::steady_clock::time_point cycledAt = std::chrono::steady_clock::now();
+  while(true)
+    {
+        std::chrono::steady_clock::time_point now =  std::chrono::steady_clock::now();
+        int elapsedSinceLastCycle = std::chrono::duration_cast<std::chrono::seconds>(now - cycledAt).count();
+        int cycleDurationInSecond = distr(engine);
+
+        if(elapsedSinceLastCycle >= cycleDurationInSecond)
+        {
+          UpdateLocation();
+          cycledAt = std::chrono::steady_clock::now();
+        }
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    }
 }
--- src/food.h
+++ src/food.h
@@ -2,17 +2,22 @@
 #define FOOD_H
 
 #include <random>
+#include <thread>
 #include <vector>
 #include "SDL.h"
 #include "snake.h"
 
-class Food {
- public:
+class Food
+{
+public:
   Food(int grid_width, int grid_height, std::shared_ptr<Snake> snake_)
     : snake(snake_),
       engine(dev()),
       random_w(0, grid_width),
-      random_h(0, grid_height){}
+      random_h(0, grid_height)  {
+    threads.emplace_back(std::thread(&Food::RandomLocationUpdate, this));
+  }
+  ~Food(){}
   void UpdateLocation();
   bool IsAt(int, int);
   SDL_Point GetLocation();
@@ -20,10 +25,14 @@ class Food {
 private:
   SDL_Point location;
   std::shared_ptr<Snake> snake;
+  std::vector<std::thread> threads;
+  std::mutex mtx;
 
   std::random_device dev;
   std::mt19937 engine;
   std::uniform_int_distribution<int> random_w;
   std::uniform_int_distribution<int> random_h;
+
+  void RandomLocationUpdate();
 };
 #endif
